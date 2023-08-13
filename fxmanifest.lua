d8d140cb6186dea48260fa0776c4fb3fc3c6c638
HeyTax <77160060+HeyTax@users.noreply.github.com>
2023-08-13 17:50:49 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'gta5'
+author 'HeyTax'
+description 'Bike Idle Animation'
+version '1.0.0'
+
+files {
+    'conditionalanims.meta',
+}
+
+data_file 'CONDITIONAL_ANIMS_FILE' 'conditionalanims.meta'
